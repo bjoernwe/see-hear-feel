bc5c350022ba712f088f9cb000f0ca7eae18b05c
Björn <bjoernwe@users.noreply.github.com>
2022-09-14 17:41:41 +0200

    Show key event on display

--- app/src/main/java/dev/upaya/shf/MainActivity.kt
+++ app/src/main/java/dev/upaya/shf/MainActivity.kt
@@ -4,8 +4,10 @@ import android.os.Bundle
 import android.view.KeyEvent
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import dev.upaya.shf.ui.MainContent
+import androidx.lifecycle.ViewModelProvider
 import timber.log.Timber
+
+import dev.upaya.shf.ui.MainContent
 
 
 class MainActivity : ComponentActivity() {
@@ -16,7 +18,9 @@ class MainActivity : ComponentActivity() {
     }
 
     override fun onKeyUp(keyCode: Int, event: KeyEvent?): Boolean {
-        Timber.tag("foo").i("Key pressed: %s", KeyEvent.keyCodeToString(keyCode))
+        val keyCodeStr: String = KeyEvent.keyCodeToString(keyCode)
+        Timber.tag("foo").i("Key pressed: %s", keyCodeStr)
+        ViewModelProvider(this)[SHFViewModel::class.java].setKey(keyCodeStr)
         return super.onKeyUp(keyCode, event)
     }
 
