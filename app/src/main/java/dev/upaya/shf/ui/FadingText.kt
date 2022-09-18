224c971a8f198f6072fdcb785ebb789498b4e674
Björn <bjoernwe@users.noreply.github.com>
2022-09-18 23:48:24 +0200

    Move fading text to composable

---
+++ app/src/main/java/dev/upaya/shf/ui/FadingText.kt
@@ -0,0 +1,41 @@
+package dev.upaya.shf.ui
+
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.spring
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+
+
+@Composable
+fun FadingText(text: String) {
+
+    val alpha = remember { Animatable(1f) }
+
+    LaunchedEffect(text) {
+        alpha.snapTo(1f)
+        alpha.animateTo(0f, animationSpec = spring(stiffness = Spring.StiffnessVeryLow))
+    }
+
+    Text(
+        text = text,
+        fontSize = 50.sp,
+        textAlign = TextAlign.Center,
+        modifier = Modifier.alpha(alpha.value)
+    )
+
+}
+
+
+@Preview
+@Composable
+fun FadingTextPreview() {
+    FadingText(text = "fading text")
+}
--- app/src/main/java/dev/upaya/shf/ui/MainContent.kt
+++ app/src/main/java/dev/upaya/shf/ui/MainContent.kt
@@ -1,21 +1,12 @@
 package dev.upaya.shf.ui
 
-import androidx.compose.animation.core.Animatable
-import androidx.compose.animation.core.Spring
-import androidx.compose.animation.core.spring
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.alpha
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 
 import dev.upaya.shf.SHFViewModel
@@ -26,12 +17,6 @@ import dev.upaya.shf.ui.theme.SHFTheme
 fun MainContent() {
 
     val viewModel: SHFViewModel = viewModel()
-    val alpha = remember { Animatable(1f) }
-
-    LaunchedEffect(viewModel.shfEvent) {
-        alpha.snapTo(1f)
-        alpha.animateTo(0f, animationSpec = spring(stiffness = Spring.StiffnessVeryLow))
-    }
 
     Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
 
@@ -40,12 +25,7 @@ fun MainContent() {
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally,
         ) {
-            Text(
-                text = viewModel.shfEvent ?: "",
-                fontSize = 50.sp,
-                textAlign = TextAlign.Center,
-                modifier = Modifier.alpha(alpha.value)
-            )
+            FadingText(text = viewModel.shfEvent ?: "")
         }
 
     }
