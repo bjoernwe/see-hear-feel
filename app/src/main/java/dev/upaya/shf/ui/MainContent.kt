1446631fec5cd35b47b913f43aa9b772052463cd
Björn <bjoernwe@users.noreply.github.com>
2022-09-19 17:37:15 +0200

    Add ripple effect

--- app/src/main/java/dev/upaya/shf/ui/MainContent.kt
+++ app/src/main/java/dev/upaya/shf/ui/MainContent.kt
@@ -1,12 +1,18 @@
 package dev.upaya.shf.ui
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.interaction.PressInteraction
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.runtime.Composable
+import androidx.compose.material.ripple.rememberRipple
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 
 import dev.upaya.shf.SHFViewModel
@@ -17,11 +23,25 @@ import dev.upaya.shf.ui.theme.SHFTheme
 fun MainContent() {
 
     val viewModel: SHFViewModel = viewModel()
+    val interactionSource = remember { MutableInteractionSource() }
+
+    // Simulate a key press on value change
+    LaunchedEffect(viewModel.lastSHFLabel) {
+        val press = PressInteraction.Press(Offset.Zero)
+        interactionSource.emit(press)
+        interactionSource.emit(PressInteraction.Release(press))
+    }
 
     Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
 
         Column(
-            Modifier.fillMaxHeight(),
+            Modifier
+                .fillMaxHeight()
+                .clickable(
+                    onClick = {},
+                    interactionSource = interactionSource,
+                    indication = rememberRipple(bounded = false, radius = 100.dp),
+                ),
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally,
         ) {
