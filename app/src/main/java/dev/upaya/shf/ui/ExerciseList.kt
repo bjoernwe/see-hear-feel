e54209367a571c1405076c7e91812ceb18492895
Björn <bjoernwe@users.noreply.github.com>
2022-09-20 19:50:42 +0200

    Fix background color

--- app/src/main/java/dev/upaya/shf/ui/ExerciseList.kt
+++ app/src/main/java/dev/upaya/shf/ui/ExerciseList.kt
@@ -2,6 +2,7 @@ package dev.upaya.shf.ui
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -11,43 +12,51 @@ import dev.upaya.shf.ui.theme.SHFTheme
 
 @Composable
 fun ExerciseList() {
-    Column(modifier = Modifier.fillMaxWidth()) {
-        ExerciseEntry(
-            title = "SHF (steady)",
-            description = "See/Hear/Feel while keeping a steady pace"
-        )
-        ExerciseEntry(
-            title = "SHF (deep)",
-            description = "See/Hear/Feel with a focus on deeply \"soaking in\"",
-        )
-        ExerciseEntry(
-            title = "Valence",
-            description = "Pleasant / Unpleasant / Neutral",
-        )
-        ExerciseEntry(
-            title = "Mahasi-Style - Basic I",
-            description = "Rising/Falling for each breath",
-        )
-        ExerciseEntry(
-            title = "Mahasi-Style - Basic II",
-            description = "Like \"Basic I\" plus noting every distraction",
-        )
-        ExerciseEntry(
-            title = "Do Nothing",
-            description = "Any intention to control attention is let go of at soon as it is noticed",
-        )
-        ExerciseEntry(
-            title = "Focus on Rest",
-            description = "SHF with focus on inner and outer restful states",
-        )
+
+    Surface {
+
+        Column(modifier = Modifier.fillMaxWidth()) {
+
+            ExerciseEntry(
+                title = "SHF (steady)",
+                description = "See/Hear/Feel while keeping a steady pace"
+            )
+            ExerciseEntry(
+                title = "SHF (deep)",
+                description = "See/Hear/Feel with a focus on deeply \"soaking in\"",
+            )
+            ExerciseEntry(
+                title = "Valence",
+                description = "Pleasant / Unpleasant / Neutral",
+            )
+            ExerciseEntry(
+                title = "Mahasi-Style - Basic I",
+                description = "Rising/Falling for each breath",
+            )
+            ExerciseEntry(
+                title = "Mahasi-Style - Basic II",
+                description = "Like \"Basic I\" plus noting every distraction",
+            )
+            ExerciseEntry(
+                title = "Do Nothing",
+                description = "Any intention to control attention is let go of at soon as it is noticed",
+            )
+            ExerciseEntry(
+                title = "Focus on Rest",
+                description = "SHF with focus on inner and outer restful states",
+            )
+
+        }
+
     }
+
 }
 
 
 @Preview
 @Composable
 fun ExerciseListPreview() {
-    SHFTheme {
+    SHFTheme(darkTheme = true) {
         ExerciseList()
     }
 }
