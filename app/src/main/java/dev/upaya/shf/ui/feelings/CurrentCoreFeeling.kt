b36868a5615d62ad642e64a4e98122b763757a41
Björn <bjoernwe@users.noreply.github.com>
2022-10-17 21:44:00 +0200

    Animate transition

--- app/src/main/java/dev/upaya/shf/ui/feelings/CurrentCoreFeeling.kt
+++ app/src/main/java/dev/upaya/shf/ui/feelings/CurrentCoreFeeling.kt
@@ -1,12 +1,13 @@
 package dev.upaya.shf.ui.feelings
 
+import androidx.compose.animation.*
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.sp
@@ -16,6 +17,7 @@ import dev.upaya.shf.ui.session.FadingText
 import dev.upaya.shf.ui.varelaFontFamily
 
 
+@OptIn(ExperimentalAnimationApi::class)
 @Composable
 fun CurrentCoreFeeling(
     currentCoreFeeling: String?,
@@ -38,20 +40,22 @@ fun CurrentCoreFeeling(
             color = MaterialTheme.colors.secondary,
         )
 
-        Text(
-            text = currentCoreFeeling?.uppercase() ?: "",
-            fontSize = 40.sp,
-            fontFamily = varelaFontFamily,
-            color = MaterialTheme.colors.secondary,
-        )
+        AnimatedContent(
+            targetState = currentCoreFeeling,
+        ) { targetCoreFeeling ->
+            Text(
+                text = targetCoreFeeling?.uppercase() ?: "",
+                fontSize = 40.sp,
+                fontFamily = varelaFontFamily,
+                color = MaterialTheme.colors.secondary,
+            )
+        }
 
         FadingText(
             text = "(${(label?.primary ?: "").lowercase()})",
             key = inputEvent,
             fontSize = 15.sp,
         )
-    }
-
     }
 
 }
