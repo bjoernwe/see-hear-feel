1792b808a41e2833282efc5d7a3b895a5d4cee3d
Björn <bjoernwe@users.noreply.github.com>
2022-10-18 13:52:50 +0200

    Add ripple

--- app/src/main/java/dev/upaya/shf/ui/feelings/CurrentCoreFeeling.kt
+++ app/src/main/java/dev/upaya/shf/ui/feelings/CurrentCoreFeeling.kt
@@ -2,18 +2,23 @@ package dev.upaya.shf.ui.feelings
 
 import androidx.compose.animation.*
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
+import androidx.compose.material.ripple.rememberRipple
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import dev.upaya.shf.exercises.labels.Label
 import dev.upaya.shf.inputs.InputEvent
 import dev.upaya.shf.ui.session.FadingText
+import dev.upaya.shf.ui.simulatePress
 import dev.upaya.shf.ui.varelaFontFamily
 
 
@@ -25,10 +30,19 @@ fun CurrentCoreFeeling(
     label: Label?
 ) {
 
+    val interactionSource = remember { MutableInteractionSource() }.apply {
+        LaunchedEffect(inputEvent) { simulatePress() }
+    }
+
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .background(MaterialTheme.colors.background),
+            .background(MaterialTheme.colors.background)
+            .clickable(
+                onClick = {},
+                interactionSource = interactionSource,
+                indication = rememberRipple(bounded = false, radius = 100.dp),
+            ),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally,
     ) {
