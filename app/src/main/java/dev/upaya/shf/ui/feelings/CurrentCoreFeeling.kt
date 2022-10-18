6dd0d867e7ce338704af554f1172c7bdf4726a61
Björn <bjoernwe@users.noreply.github.com>
2022-10-18 14:35:21 +0200

    New layout

--- app/src/main/java/dev/upaya/shf/ui/feelings/CurrentCoreFeeling.kt
+++ app/src/main/java/dev/upaya/shf/ui/feelings/CurrentCoreFeeling.kt
@@ -1,28 +1,27 @@
 package dev.upaya.shf.ui.feelings
 
-import androidx.compose.animation.*
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.material.ripple.rememberRipple
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import dev.upaya.shf.exercises.labelmaps.LabelMapKeepDiscard
 import dev.upaya.shf.exercises.labels.Label
 import dev.upaya.shf.inputs.InputEvent
 import dev.upaya.shf.ui.session.FadingText
 import dev.upaya.shf.ui.simulatePress
+import dev.upaya.shf.ui.theme.SHFTheme
 import dev.upaya.shf.ui.varelaFontFamily
 
 
-@OptIn(ExperimentalAnimationApi::class)
 @Composable
 fun CurrentCoreFeeling(
     currentCoreFeeling: String?,
@@ -47,29 +46,55 @@ fun CurrentCoreFeeling(
         horizontalAlignment = Alignment.CenterHorizontally,
     ) {
 
-        Text(
-            text = "\"I feel ...\"",
-            fontSize = 20.sp,
-            fontFamily = varelaFontFamily,
-            color = MaterialTheme.colors.secondary,
-        )
-
-        AnimatedContent(
-            targetState = currentCoreFeeling,
-        ) { targetCoreFeeling ->
+        Column(
+            modifier = Modifier.weight(1f),
+            verticalArrangement = Arrangement.Bottom,
+        ) {
             Text(
-                text = targetCoreFeeling?.uppercase() ?: "",
-                fontSize = 40.sp,
+                text = "\"I feel ...\"",
+                fontSize = 20.sp,
                 fontFamily = varelaFontFamily,
                 color = MaterialTheme.colors.secondary,
             )
         }
 
-        FadingText(
-            text = "(${(label?.primary ?: "").lowercase()})",
-            key = inputEvent,
-            fontSize = 15.sp,
+        Text(
+            text = currentCoreFeeling?.uppercase() ?: "",
+            fontSize = 40.sp,
+            fontFamily = varelaFontFamily,
+            color = MaterialTheme.colors.secondary,
         )
+
+        Column(
+            modifier = Modifier.weight(1f),
+            verticalArrangement = Arrangement.Bottom,
+        ) {
+            FadingText(
+                text = when (label)
+                {
+                    LabelMapKeepDiscard.labelKeep -> "strong resonance"
+                    LabelMapKeepDiscard.labelDiscard -> "weak resonance"
+                    else -> ""
+                },
+                modifier = Modifier.padding(bottom = 30.dp),
+                fontSize = 20.sp,
+                key = inputEvent,
+            )
+        }
+
     }
 
+}
+
+
+@Preview
+@Composable
+fun CurrentCoreFeelingPreview() {
+    SHFTheme(darkTheme = true) {
+        CurrentCoreFeeling(
+            currentCoreFeeling = "Core Feeling",
+            inputEvent = null,
+            label = LabelMapKeepDiscard.labelKeep,
+        )
+    }
 }
