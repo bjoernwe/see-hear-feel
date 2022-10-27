84bc3f756ddbb827f242a894deba6d364a73b0d7
Björn <bjoernwe@users.noreply.github.com>
2022-10-27 18:23:35 +0200

    Enable preview

--- app/src/main/java/dev/upaya/shf/ui/SHFNavHost.kt
+++ app/src/main/java/dev/upaya/shf/ui/SHFNavHost.kt
@@ -1,6 +1,8 @@
 package dev.upaya.shf.ui
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
@@ -9,6 +11,7 @@ import androidx.navigation.compose.composable
 import dev.upaya.shf.ui.exercises.SessionViewModel
 import dev.upaya.shf.exercises.exerciselist.ExerciseRoute
 import dev.upaya.shf.ui.exercises.ExerciseListScreen
+import dev.upaya.shf.ui.exercises.ExerciseListViewModel
 import dev.upaya.shf.ui.feelings.CoreFeelingScreen
 import dev.upaya.shf.ui.session.SessionScreen
 
@@ -28,7 +31,9 @@ fun SHFNavHost(
     ) {
 
         composable(route = "exercises") {
-            ExerciseListScreen { cfg ->
+            val exerciseListViewModel: ExerciseListViewModel = hiltViewModel()
+            val exercises by exerciseListViewModel.exercises.collectAsState()
+            ExerciseListScreen(exercises = exercises) { cfg ->
                 sessionViewModel.setCurrentExercise(exerciseConfig = cfg)
                 navController.navigate(cfg.route.name)
             }
