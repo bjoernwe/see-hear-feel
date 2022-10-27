59aa64a17fcaca331e55b7fbf59bf8bb12bedc46
Björn <bjoernwe@users.noreply.github.com>
2022-10-27 18:23:35 +0200

    Enable preview

--- app/src/main/java/dev/upaya/shf/ui/exercises/ExerciseListScreen.kt
+++ app/src/main/java/dev/upaya/shf/ui/exercises/ExerciseListScreen.kt
@@ -2,28 +2,26 @@ package dev.upaya.shf.ui.exercises
 
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.hilt.navigation.compose.hiltViewModel
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import dev.upaya.shf.exercises.exerciselist.ExerciseConfig
+import dev.upaya.shf.exercises.exerciselist.exampleExercises
 import dev.upaya.shf.ui.ExerciseEntry
 import dev.upaya.shf.ui.theme.SHFTheme
 
 
 @Composable
 fun ExerciseListScreen(
-    viewModel: ExerciseListViewModel = hiltViewModel(),
+    exercises: List<ExerciseConfig>,
     onClick: (ExerciseConfig) -> Unit = {},
 ) {
 
-    val exercises by viewModel.exercises.collectAsState()
     val systemUiController = rememberSystemUiController()
 
     systemUiController.setStatusBarColor(color = MaterialTheme.colors.secondaryVariant)
@@ -35,9 +33,11 @@ fun ExerciseListScreen(
                 backgroundColor = MaterialTheme.colors.secondary,
             )
         }
-    ) {
+    ) { padding ->
         Surface(
-            modifier = Modifier.fillMaxSize()
+            modifier = Modifier
+                .padding(padding)
+                .fillMaxSize()
         ) {
             LazyColumn(modifier = Modifier.fillMaxWidth()) {
                 items(exercises) { cfg ->
@@ -53,6 +53,8 @@ fun ExerciseListScreen(
 @Composable
 fun ExerciseListPreview() {
     SHFTheme(darkTheme = true) {
-        ExerciseListScreen()
+        ExerciseListScreen(
+            exercises = exampleExercises,
+        )
     }
 }
