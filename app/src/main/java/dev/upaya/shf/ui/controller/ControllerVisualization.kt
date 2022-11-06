0c462fbe528eac16e77508cf399653a5201bbdfc
Björn <bjoernwe@users.noreply.github.com>
2022-11-06 19:36:22 +0100

    Refactor controller composable

--- app/src/main/java/dev/upaya/shf/ui/controller/ControllerVisualization.kt
+++ app/src/main/java/dev/upaya/shf/ui/controller/ControllerVisualization.kt
@@ -9,6 +9,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.drawWithContent
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.drawscope.ContentDrawScope
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import dev.upaya.shf.R
@@ -30,46 +31,82 @@ fun ControllerVisualization(
         contentDescription = "Game Controller Outline",
         modifier = modifier
             .drawWithContent {
-
-                val buttonRadius = this.size.minDimension / 35f
-                val buttonColorInactive = Color.Black
-
                 drawContent()
-
-                drawCircle(
-                    color = if (inputEvent?.inputKey == InputKey.KEY_Y) Color.Green else buttonColorInactive,
-                    radius = buttonRadius,
-                    center = this.center + Offset(
-                        0.146f * this.size.minDimension,
-                        -0.168f * this.size.minDimension,
+                inputEvent?.inputKey?.let { inputKey ->
+                    drawButtons(
+                        contentDrawScope = this,
+                        inputKey = inputKey,
                     )
-                )
-                drawCircle(
-                    color = if (inputEvent?.inputKey == InputKey.KEY_X) Color.Green else buttonColorInactive,
-                    radius = buttonRadius,
-                    center = this.center + Offset(
-                        0.063f * this.size.minDimension,
-                        -0.084f * this.size.minDimension,
-                    )
-                )
-                drawCircle(
-                    color = if (inputEvent?.inputKey == InputKey.KEY_B) Color.Green else buttonColorInactive,
-                    radius = buttonRadius,
-                    center = this.center + Offset(
-                        0.23f * this.size.minDimension,
-                        -0.084f * this.size.minDimension,
-                    )
-                )
-                drawCircle(
-                    color = if (inputEvent?.inputKey == InputKey.KEY_A) Color.Green else buttonColorInactive,
-                    radius = buttonRadius,
-                    center = this.center + Offset(
-                        0.146f * this.size.minDimension,
-                        -0.001f * this.size.minDimension,
-                    )
-                )
+                }
             }
     )
+}
+
+
+private fun drawButtons(
+    contentDrawScope: ContentDrawScope,
+    inputKey: InputKey,
+) {
+    drawButton(
+        contentDrawScope = contentDrawScope,
+        inputKey = inputKey,
+        targetKey = InputKey.KEY_A,
+    )
+    drawButton(
+        contentDrawScope = contentDrawScope,
+        inputKey = inputKey,
+        targetKey = InputKey.KEY_B,
+    )
+    drawButton(
+        contentDrawScope = contentDrawScope,
+        inputKey = inputKey,
+        targetKey = InputKey.KEY_X,
+    )
+    drawButton(
+        contentDrawScope = contentDrawScope,
+        inputKey = inputKey,
+        targetKey = InputKey.KEY_Y,
+    )
+}
+
+
+private fun buttonOffset(inputKey: InputKey): Offset? {
+    return when (inputKey) {
+        InputKey.KEY_A -> Offset(0.146f, -0.001f)
+        InputKey.KEY_B -> Offset(0.23f, -0.084f)
+        InputKey.KEY_X -> Offset(0.063f, -0.084f)
+        InputKey.KEY_Y -> Offset(0.146f, -0.168f)
+        InputKey.KEY_UP -> null
+        InputKey.KEY_DOWN -> null
+        InputKey.KEY_LEFT -> null
+        InputKey.KEY_RIGHT -> null
+        InputKey.KEY_L1 -> null
+        InputKey.KEY_R1 -> null
+        InputKey.KEY_L2 -> null
+        InputKey.KEY_R2 -> null
+        InputKey.UNMAPPED -> null
+    }
+}
+
+
+private fun drawButton(
+    contentDrawScope: ContentDrawScope,
+    inputKey: InputKey,
+    targetKey: InputKey,
+    buttonRadiusDivider: Float = 35f,
+    buttonColorActive: Color = Color.Green,
+    buttonColorInactive: Color = Color.Black,
+) {
+    buttonOffset(targetKey)?.let { buttonOffset ->
+        contentDrawScope.drawCircle(
+            color = if (inputKey == targetKey) buttonColorActive else buttonColorInactive,
+            radius = contentDrawScope.size.minDimension / buttonRadiusDivider,
+            center = contentDrawScope.center + Offset(
+                x = buttonOffset.x * contentDrawScope.size.minDimension,
+                y = buttonOffset.y * contentDrawScope.size.minDimension,
+            )
+        )
+    }
 }
 
 
