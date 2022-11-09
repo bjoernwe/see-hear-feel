a41a1e68965fa8b92c2215ed3c3ded47f4962cc6
Björn <bjoernwe@users.noreply.github.com>
2022-11-09 14:09:40 +0100

    Adjust layout

--- app/src/main/java/dev/upaya/shf/ui/controller/ControllerSetupScreen.kt
+++ app/src/main/java/dev/upaya/shf/ui/controller/ControllerSetupScreen.kt
@@ -2,8 +2,8 @@ package dev.upaya.shf.ui.controller
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import dev.upaya.shf.inputs.InputKey
@@ -14,8 +14,11 @@ import dev.upaya.shf.ui.theme.SHFTheme
 fun ControllerSetupScreen(
     inputKey: InputKey?,
 ) {
-    Column() {
-        Text("Controller Setup")
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier
+            .fillMaxSize(),
+    ) {
         ControllerVisualization(
             inputKey = inputKey,
             modifier = Modifier
