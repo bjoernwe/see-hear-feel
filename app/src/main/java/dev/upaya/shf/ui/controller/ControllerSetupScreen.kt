b212ef4d5b250f701fe61cfcb62ee85968c22e31
Björn <bjoernwe@users.noreply.github.com>
2022-11-09 22:00:25 +0100

    Fix background

--- app/src/main/java/dev/upaya/shf/ui/controller/ControllerSetupScreen.kt
+++ app/src/main/java/dev/upaya/shf/ui/controller/ControllerSetupScreen.kt
@@ -1,7 +1,9 @@
 package dev.upaya.shf.ui.controller
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
@@ -24,6 +26,7 @@ fun ControllerSetupScreen(
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
         modifier = Modifier
+            .background(MaterialTheme.colors.background)
             .fillMaxSize(),
     ) {
 
