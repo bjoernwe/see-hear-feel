11c17ecf63dc6ee4dd9a915ec33e2a277f0aaf1b
Björn <bjoernwe@users.noreply.github.com>
2022-11-13 15:52:36 +0100

    Remove unused flow

--- app/src/main/java/dev/upaya/shf/session/ActiveSessionSource.kt
+++ app/src/main/java/dev/upaya/shf/session/ActiveSessionSource.kt
@@ -1,10 +1,8 @@
 package dev.upaya.shf.session
 
 import dev.upaya.shf.exercises.exerciselist.ExerciseConfig
-import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.transform
 import javax.inject.Inject
 import javax.inject.Singleton
 
@@ -14,10 +12,6 @@ class ActiveSessionSource @Inject constructor() {
 
     private val _exerciseConfig: MutableStateFlow<ExerciseConfig?> = MutableStateFlow(null)
     val exerciseConfig: StateFlow<ExerciseConfig?> = _exerciseConfig
-
-    val sessionActive: Flow<Boolean> = exerciseConfig.transform { cfg ->
-        emit(cfg != null)
-    }
 
     fun beginSession(exerciseConfig: ExerciseConfig) {
         _exerciseConfig.value = exerciseConfig
