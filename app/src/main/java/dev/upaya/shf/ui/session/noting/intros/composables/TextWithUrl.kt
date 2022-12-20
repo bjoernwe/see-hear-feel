24245b2e533bc92a80bbd04093cb40af0a988a03
Björn <bjoernwe@users.noreply.github.com>
2022-12-20 14:47:33 +0100

    Add TextWithUrl

---
+++ app/src/main/java/dev/upaya/shf/ui/session/noting/intros/composables/TextWithUrl.kt
@@ -0,0 +1,78 @@
+package dev.upaya.shf.ui.session.noting.intros.composables
+
+import androidx.compose.foundation.text.ClickableText
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.tooling.preview.Preview
+import dev.upaya.shf.ui.theme.SHFTheme
+import timber.log.Timber
+
+
+private const val tagUrl = "URL"
+
+
+@Composable
+fun TextWithUrl(
+    onClick: (String) -> Unit = {
+        Timber.tag("Clicked URL").d(it)
+    },
+    builder: AnnotatedString.Builder.() -> Unit,
+) {
+
+    val annotatedText = buildAnnotatedString(builder = builder)
+
+    ClickableText(
+        text = annotatedText,
+        onClick = { offset ->
+            annotatedText.getStringAnnotations(
+                tag = tagUrl,
+                start = offset,
+                end = offset
+            ).firstOrNull()?.let { annotation ->
+                onClick(annotation.item)
+            }
+        }
+    )
+
+}
+
+
+private fun AnnotatedString.Builder.appendUrl(
+    text: String,
+    url: String,
+) {
+
+    pushStringAnnotation(
+        tag = tagUrl,
+        annotation = url,
+    )
+
+    withStyle(
+        style = SpanStyle(color = Color.Blue, fontWeight = FontWeight.Bold)
+    ) {
+        append(text)
+    }
+
+    pop()
+
+}
+
+
+@Preview
+@Composable
+fun TextWithUrlPreview() {
+    SHFTheme(darkTheme = true) {
+        TextWithUrl() {
+            append("Click ")
+            appendUrl(
+                text = "here",
+                url = "http://www.google.com",
+            )
+        }
+    }
+}
