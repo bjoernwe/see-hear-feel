6e37243e8e378d39f02ea0382e33f7d126e1beb6
Björn <bjoernwe@users.noreply.github.com>
2023-02-02 20:11:25 +0100

    Extract navigation graph build

---
+++ app/src/main/java/dev/upaya/shf/ui/session/noting/session/NotingSessionNavigation.kt
@@ -0,0 +1,50 @@
+package dev.upaya.shf.ui.session.noting.session
+
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.navigation.NavController
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.compose.composable
+import dev.upaya.shf.exercises.labels.Label
+import dev.upaya.shf.ui.session.noting.NotingScreen
+import dev.upaya.shf.ui.session.noting.SessionViewModel
+import dev.upaya.shf.ui.session.noting.getScopedSessionViewModel
+import dev.upaya.shf.ui.session.noting.routeNotingGraphWithArg
+
+
+internal const val routeNotingSession = "noting_session"
+
+
+internal fun NavGraphBuilder.notingSessionScreen(
+    navController: NavController,
+    onStopButtonClick: () -> Unit,
+) {
+
+    composable(routeNotingSession) { backStackEntry ->
+
+        val sessionViewModel: SessionViewModel = getScopedSessionViewModel(
+            routeForScope = routeNotingGraphWithArg,
+            backStackEntry = backStackEntry,
+            navController = navController
+        )
+
+        val label: Label by sessionViewModel.labelFlow.collectAsState(initial = Label(""))
+        val inputEvent by sessionViewModel.inputEventFlow.collectAsState(initial = null)
+
+        DisposableEffect(sessionViewModel) {
+            sessionViewModel.startStatsCollection()
+            onDispose {
+                sessionViewModel.stopStatsCollection()
+            }
+        }
+
+        NotingScreen(
+            label = label,
+            inputEvent = inputEvent,
+            onStopButtonClick = onStopButtonClick,
+        )
+
+    }
+
+}
