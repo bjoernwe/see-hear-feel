3c04334f23a7aad631879a1079e97887bcadbf1b
Björn <bjoernwe@users.noreply.github.com>
2023-07-20 20:42:13 +0200

    Vibrate on background click

--- app/src/main/java/dev/upaya/shf/background/ForegroundNotificationService.kt
+++ app/src/main/java/dev/upaya/shf/background/ForegroundNotificationService.kt
@@ -8,12 +8,20 @@ import android.app.Service
 import android.content.Context
 import android.content.Intent
 import android.os.Binder
+import android.os.Build
 import android.os.IBinder
+import android.os.VibrationEffect
+import android.os.Vibrator
+import android.os.VibratorManager
 import androidx.core.app.NotificationCompat
 import dagger.hilt.android.AndroidEntryPoint
 import dev.upaya.shf.R
 import dev.upaya.shf.SHFActivity
 import dev.upaya.shf.inputs.InputKeySource
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 
@@ -28,6 +36,11 @@ class ForegroundNotificationService : Service() {
 
     private val binder = LocalBinder()  // Allows an Activity to bind to this service
 
+    private val job = SupervisorJob()
+    private val scope = CoroutineScope(Dispatchers.IO + job)
+
+    private val vibrationEffect: VibrationEffect = VibrationEffect.createPredefined(VibrationEffect.EFFECT_HEAVY_CLICK)
+
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         showForegroundNotification()
         return super.onStartCommand(intent, flags, startId)
@@ -37,6 +50,9 @@ class ForegroundNotificationService : Service() {
         registerNotificationChannel()
         val notification = createNotification()
         startForeground(ONGOING_NOTIFICATION_ID, notification)
+        scope.launch {
+            inputKeySource.inputKeyDown.collect { vibrate() }
+        }
     }
 
     private fun createNotification(): Notification {
@@ -74,5 +90,24 @@ class ForegroundNotificationService : Service() {
 
     override fun onBind(intend: Intent): IBinder {
         return binder
+    }
+
+    private fun vibrate() {
+        getVibrator().vibrate(vibrationEffect)
+    }
+
+    private fun getVibrator(): Vibrator {
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            val vibratorManager = getSystemService(Context.VIBRATOR_MANAGER_SERVICE) as VibratorManager
+            vibratorManager.defaultVibrator
+        } else {
+            @Suppress("DEPRECATION")
+            getSystemService(VIBRATOR_SERVICE) as Vibrator
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        job.cancel()
     }
 }
