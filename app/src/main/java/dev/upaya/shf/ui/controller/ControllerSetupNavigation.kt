fdfaa44e93abf5a24094fe597157b4538d422ea9
Björn <bjoernwe@users.noreply.github.com>
2023-07-22 22:18:59 +0200

    Enable inputs for controller setup

--- app/src/main/java/dev/upaya/shf/ui/controller/ControllerSetupNavigation.kt
+++ app/src/main/java/dev/upaya/shf/ui/controller/ControllerSetupNavigation.kt
@@ -1,11 +1,13 @@
 package dev.upaya.shf.ui.controller
 
+import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
+import dev.upaya.shf.inputs.input_keys.GlobalInputRegistrarViewModel
 
 
 private const val routeControllerSetup = "controller"
@@ -22,6 +24,13 @@ fun NavGraphBuilder.controllerSetupScreen() {
 
         val controllerSetupViewModel: ControllerSetupViewModel = hiltViewModel()
         val keyPressStates by controllerSetupViewModel.keyPressStates.collectAsState()
+
+        val globalInputRegistrarViewModel: GlobalInputRegistrarViewModel = hiltViewModel()
+
+        DisposableEffect(globalInputRegistrarViewModel) {
+            globalInputRegistrarViewModel.switchOn()
+            onDispose { globalInputRegistrarViewModel.switchOff() }
+        }
 
         ControllerSetupScreen(
             keyPressStates = keyPressStates,
