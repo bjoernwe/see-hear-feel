fba8d08bcc580aa95019d8e6652bfc20697a6d47
Björn <bjoernwe@users.noreply.github.com>
2023-07-22 22:20:56 +0200

    Rename module

--- app/src/main/java/dev/upaya/shf/inputs/key_press_states/KeyPressStateViewModel.kt
+++ app/src/main/java/dev/upaya/shf/inputs/key_press_states/KeyPressStateViewModel.kt
@@ -1,15 +1,13 @@
-package dev.upaya.shf.ui.controller
+package dev.upaya.shf.inputs.key_press_states
 
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
-import dev.upaya.shf.inputs.key_press_states.KeyPressStateSource
-import dev.upaya.shf.inputs.key_press_states.KeyPressStates
 import kotlinx.coroutines.flow.StateFlow
 import javax.inject.Inject
 
 
 @HiltViewModel
-class ControllerSetupViewModel @Inject constructor(
+class KeyPressStateViewModel @Inject constructor(
     keyPressStateSource: KeyPressStateSource,
 ) : ViewModel() {
     val keyPressStates: StateFlow<KeyPressStates> = keyPressStateSource.keyPressStates
--- app/src/main/java/dev/upaya/shf/ui/controller/ControllerSetupNavigation.kt
+++ app/src/main/java/dev/upaya/shf/ui/controller/ControllerSetupNavigation.kt
@@ -8,6 +8,7 @@ import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
 import dev.upaya.shf.inputs.input_keys.GlobalInputRegistrarViewModel
+import dev.upaya.shf.inputs.key_press_states.KeyPressStateViewModel
 
 
 private const val routeControllerSetup = "controller"
@@ -22,10 +23,9 @@ fun NavGraphBuilder.controllerSetupScreen() {
 
     composable(route = routeControllerSetup) {
 
-        val controllerSetupViewModel: ControllerSetupViewModel = hiltViewModel()
-        val keyPressStates by controllerSetupViewModel.keyPressStates.collectAsState()
-
         val globalInputRegistrarViewModel: GlobalInputRegistrarViewModel = hiltViewModel()
+        val keyPressStateViewModel: KeyPressStateViewModel = hiltViewModel()
+        val keyPressStates by keyPressStateViewModel.keyPressStates.collectAsState()
 
         DisposableEffect(globalInputRegistrarViewModel) {
             globalInputRegistrarViewModel.switchOn()
