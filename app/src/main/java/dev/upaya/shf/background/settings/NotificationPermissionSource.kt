e55b80aef170454bc5583cf413de35c90b7e0943
Björn <bjoernwe@users.noreply.github.com>
2023-07-30 12:18:07 +0200

    🐛 Fix initial state

--- app/src/main/java/dev/upaya/shf/background/settings/NotificationPermissionSource.kt
+++ app/src/main/java/dev/upaya/shf/background/settings/NotificationPermissionSource.kt
@@ -2,7 +2,10 @@ package dev.upaya.shf.background.settings
 
 import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.provider.Settings
+import androidx.core.content.ContextCompat
+import dagger.hilt.android.qualifiers.ApplicationContext
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import javax.inject.Inject
@@ -10,13 +13,21 @@ import javax.inject.Singleton
 
 
 @Singleton
-class NotificationPermissionSource @Inject constructor() {
+class NotificationPermissionSource @Inject constructor(
+    @ApplicationContext private val appContext: Context,
+) {
 
-    private val _hasNotificationPermission = MutableStateFlow(false)
+    private val _hasNotificationPermission = MutableStateFlow(areNotificationsEnabled())
     val hasNotificationPermission: StateFlow<Boolean> = _hasNotificationPermission
 
     fun updatePermission(hasNotificationPermission: Boolean) {
         _hasNotificationPermission.value = hasNotificationPermission
+    }
+
+    private fun areNotificationsEnabled(): Boolean {
+        return ContextCompat.checkSelfPermission(
+            appContext, android.Manifest.permission.POST_NOTIFICATIONS
+        ) == PackageManager.PERMISSION_GRANTED
     }
 
     private fun openNotificationSettings(context: Context) {
