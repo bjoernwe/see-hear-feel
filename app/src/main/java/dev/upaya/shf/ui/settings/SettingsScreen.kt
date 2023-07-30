54e39ddd1c381c4c1a56399860e448d4caa92fbe
Björn <bjoernwe@users.noreply.github.com>
2023-07-30 19:02:02 +0200

    💄 Add top bar to settings

--- app/src/main/java/dev/upaya/shf/ui/settings/SettingsScreen.kt
+++ app/src/main/java/dev/upaya/shf/ui/settings/SettingsScreen.kt
@@ -4,10 +4,13 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Switch
 import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
@@ -19,34 +22,48 @@ fun SettingsScreen(
     onSwitchLockScreenSession: (Boolean) -> Unit,
 ) {
 
-    Column {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text(text = "Settings", color = Color.White) },
+                //backgroundColor = MaterialTheme.colors.secondary,
+                //navigationIcon = { Icon(painter = painterResource(R.drawable.baseline_arrow_back_ios_24), contentDescription = "") },
+            )
+        }
+    ) { padding ->
 
-        Row(modifier = Modifier
-            .fillMaxWidth()
-            .padding(8.dp)
+        Column(
+            modifier = Modifier.padding(padding)
         ) {
 
-            Column {
+            Row(modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp)
+            ) {
 
-                Text(
-                    text = "Session on Lock-Screen",
+                Column {
+
+                    Text(
+                        text = "Session on Lock-Screen",
+                        modifier = Modifier
+                        //.fillMaxWidth()
+                    )
+
+                    Text(text = "Accessibility service: $hasAccessibilityPermission")
+
+                }
+
+                Switch(
+                    checked = isLockScreenSessionEnabled,
+                    onCheckedChange = onSwitchLockScreenSession,
                     modifier = Modifier
-                    //.fillMaxWidth()
+                        .padding(4.dp)
                 )
-
-                Text(text = "Accessibility service: $hasAccessibilityPermission")
 
             }
 
-            Switch(
-                checked = isLockScreenSessionEnabled,
-                onCheckedChange = onSwitchLockScreenSession,
-                modifier = Modifier
-                    .padding(4.dp)
-            )
 
         }
-
 
     }
 
