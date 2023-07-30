6ce1e2afe19f955ee253949fed0166cf2b3bbafa
Björn <bjoernwe@users.noreply.github.com>
2023-07-30 20:24:53 +0200

    ♻️ Do not store context

--- app/src/main/java/dev/upaya/shf/inputs/permissions/accessibility/AccessibilityPermissionSource.kt
+++ app/src/main/java/dev/upaya/shf/inputs/permissions/accessibility/AccessibilityPermissionSource.kt
@@ -1,80 +1,46 @@
 package dev.upaya.shf.inputs.permissions.accessibility
 
-import android.content.ComponentName
 import android.content.Context
 import android.database.ContentObserver
 import android.os.Handler
 import android.os.Looper
 import android.provider.Settings
 import dagger.hilt.android.qualifiers.ApplicationContext
-import dev.upaya.shf.background.BackgroundKeyEventService
 import dev.upaya.shf.inputs.permissions.IBooleanSource
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
-import timber.log.Timber
 import javax.inject.Inject
 import javax.inject.Singleton
 
 
 @Singleton
 class AccessibilityPermissionSource @Inject constructor(
-    @ApplicationContext private val appContext: Context,
+    @ApplicationContext appContext: Context,
 ) : IBooleanSource {
 
-    private val _backgroundServiceIsAvailable = MutableStateFlow(getAccessibilityServiceAvailability())
+    private val _backgroundServiceIsAvailable = MutableStateFlow(getAccessibilityServiceAvailability(context = appContext))
     override val isEnabled: StateFlow<Boolean> = _backgroundServiceIsAvailable
 
     private val accessibilityChangeObserver: ContentObserver = object : ContentObserver(Handler(Looper.getMainLooper())) {
 
         override fun onChange(selfChange: Boolean) {
             super.onChange(selfChange)
-            updateAvailability()
+            updateAvailability(context = appContext)
         }
 
-        private fun updateAvailability() {
-            _backgroundServiceIsAvailable.value = getAccessibilityServiceAvailability()
+        private fun updateAvailability(context: Context) {
+            _backgroundServiceIsAvailable.value = getAccessibilityServiceAvailability(context = context)
         }
 
     }
 
     init {
-        registerChangeObserver()
+        registerChangeObserver(context = appContext)
     }
 
-    private fun registerChangeObserver() {
+    private fun registerChangeObserver(context: Context) {
         val uri = Settings.Secure.getUriFor(Settings.Secure.ENABLED_ACCESSIBILITY_SERVICES)
-        appContext.contentResolver.registerContentObserver(uri, false, accessibilityChangeObserver)
-    }
-
-    /**
-     * Return whether or not the accessibility service is currently active.
-     */
-    private fun getAccessibilityServiceAvailability(): Boolean {
-        val accessibilityServiceComponentName = ComponentName(appContext, BackgroundKeyEventService::class.java)
-        return getEnabledAccessibilityServiceComponents().contains(accessibilityServiceComponentName)
-    }
-
-    /**
-     * Return a list of component names for all the accessibility services that are currently
-     * enabled.
-     */
-    private fun getEnabledAccessibilityServiceComponents(): List<ComponentName> {
-        val serviceString = getEnabledAccessibilityServices()
-        return serviceString
-            .split(":")
-            .mapNotNull(ComponentName::unflattenFromString)
-    }
-
-    /**
-     * Return a string from Android settings that lists all enabled accessibility services.
-     */
-    private fun getEnabledAccessibilityServices(): String {
-        try {
-            return Settings.Secure.getString(appContext.contentResolver,  Settings.Secure.ENABLED_ACCESSIBILITY_SERVICES)
-        } catch (e: Settings.SettingNotFoundException) {
-            Timber.e(e)
-        }
-        return ""
+        context.contentResolver.registerContentObserver(uri, false, accessibilityChangeObserver)
     }
 
 }
