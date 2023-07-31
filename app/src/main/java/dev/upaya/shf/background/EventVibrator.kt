d00d9e84f2a196186d02d179629232036443a122
Björn <bjoernwe@users.noreply.github.com>
2023-07-31 20:53:49 +0200

    ⚰️ Remove deprecated code

--- app/src/main/java/dev/upaya/shf/background/EventVibrator.kt
+++ app/src/main/java/dev/upaya/shf/background/EventVibrator.kt
@@ -1,8 +1,6 @@
 package dev.upaya.shf.background
 
-import android.app.Service
 import android.content.Context
-import android.os.Build
 import android.os.VibrationEffect
 import android.os.Vibrator
 import android.os.VibratorManager
@@ -29,13 +27,8 @@ class EventVibrator(
     companion object {
 
         private fun getVibrator(context: Context): Vibrator {
-            return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
-                val vibratorManager = context.getSystemService(Context.VIBRATOR_MANAGER_SERVICE) as VibratorManager
-                vibratorManager.defaultVibrator
-            } else {
-                @Suppress("DEPRECATION")
-                context.getSystemService(Service.VIBRATOR_SERVICE) as Vibrator
-            }
+            val vibratorManager = context.getSystemService(Context.VIBRATOR_MANAGER_SERVICE) as VibratorManager
+            return vibratorManager.defaultVibrator
         }
     }
 
