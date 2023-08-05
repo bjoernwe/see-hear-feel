dc88d17f43d47e5b9e3041b5de54a7d887e17c16
Björn <bjoernwe@users.noreply.github.com>
2023-08-05 19:35:18 +0200

    💄 Update settings screen

--- app/src/main/java/dev/upaya/shf/ui/settings/SettingsScreen.kt
+++ app/src/main/java/dev/upaya/shf/ui/settings/SettingsScreen.kt
@@ -3,17 +3,22 @@ package dev.upaya.shf.ui.settings
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.Icon
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Switch
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import dev.upaya.shf.R
+import dev.upaya.shf.ui.theme.SHFTheme
 
 
 @Composable
@@ -35,35 +40,81 @@ fun SettingsScreen(
     ) { padding ->
 
         Column(
-            modifier = Modifier.padding(padding)
+            modifier = Modifier
+                .padding(padding)
+                .padding(4.dp)
         ) {
 
-            Row(modifier = Modifier
-                .fillMaxWidth()
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier
+                //.fillMaxWidth()
+                //.wrapContentHeight()
                 .padding(8.dp)
             ) {
 
-                Column {
+                Icon(
+                    painter = painterResource(R.drawable.baseline_screen_lock_portrait_24),
+                    contentDescription = "Locked Screen",
+                    modifier = Modifier
+                        .size(36.dp)
+                        //.padding(8.dp)
+                        //.align(Alignment.Top)
+                        //.wrapContentSize()
+                        //.padding(8.dp)
+                )
+
+                Column(
+                    modifier = Modifier
+                        .weight(1F)
+                        .padding(start = 10.dp)
+                        .clickable { onRequestAccessibilitySettings() }
+                ) {
 
                     Text(
-                        text = "Session on Lock-Screen",
+                        text = "Session on lock screen",
                         modifier = Modifier
-                        //.fillMaxWidth()
                     )
 
-                    Text(
-                        text = "Accessibility service: $hasAccessibilityPermission",
-                        Modifier.clickable(onClick = onRequestAccessibilitySettings)
-                    )
+                    /*Row(
+                        verticalAlignment = Alignment.CenterVertically,
+                        modifier = Modifier
+                            .wrapContentHeight()
+                    ) {
+
+                        Icon(
+                            painter = painterResource(R.drawable.baseline_check_box_24),
+                            contentDescription = "check",
+                            modifier = Modifier
+                                .padding(end = 4.dp)
+                                .size(14.dp)
+                        )
+
+                        Text(
+                            text = "Requires accessibility service: ",
+                            color = Color.Gray,
+                            modifier = Modifier
+                                //.padding(start = 4.dp)
+                        )
+
+                        Text(
+                            text = "$hasAccessibilityPermission",
+                            color = Color.Gray,
+                            textDecoration = TextDecoration.Underline,
+                            modifier = Modifier
+                                .clickable(onClick = onRequestAccessibilitySettings)
+                        )
+
+                    }*/
 
                 }
 
                 Switch(
-                    checked = isLockScreenPreferred,
+                    checked = hasAccessibilityPermission && isLockScreenPreferred,
                     onCheckedChange = onSwitchLockScreenSession,
                     enabled = hasAccessibilityPermission,
                     modifier = Modifier
-                        .padding(4.dp)
+                        //.padding(4.dp)
                 )
 
             }
@@ -79,10 +130,12 @@ fun SettingsScreen(
 @Preview
 @Composable
 fun SettingsScreenPreview() {
-    SettingsScreen(
-        isLockScreenPreferred = true,
-        hasAccessibilityPermission = true,
-        onSwitchLockScreenSession = {},
-        onRequestAccessibilitySettings = {},
-    )
+    SHFTheme(darkTheme = true) {
+        SettingsScreen(
+            isLockScreenPreferred = true,
+            hasAccessibilityPermission = true,
+            onSwitchLockScreenSession = {},
+            onRequestAccessibilitySettings = {},
+        )
+    }
 }
