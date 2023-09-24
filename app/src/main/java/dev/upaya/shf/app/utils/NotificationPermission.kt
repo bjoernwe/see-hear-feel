c2edb1bfdeefe9b5c179a0f435d9c4e9f2cb0ce9
Björn <bjoernwe@users.noreply.github.com>
2023-09-24 19:39:45 +0200

    ⬇️ Make compatible with Oreo

--- app/src/main/java/dev/upaya/shf/app/utils/NotificationPermission.kt
+++ app/src/main/java/dev/upaya/shf/app/utils/NotificationPermission.kt
@@ -2,6 +2,7 @@ package dev.upaya.shf.app.utils
 
 import android.content.Context
 import android.content.pm.PackageManager
+import android.os.Build
 import androidx.activity.result.ActivityResultLauncher
 import androidx.core.content.ContextCompat
 
@@ -11,11 +12,15 @@ class NotificationPermission {
     companion object {
 
         fun requestNotificationPermissionIfNecessary(context: Context, activityResultLauncher: ActivityResultLauncher<String>) {
+            if (Build.VERSION.SDK_INT < 33)
+                return
             if (!areNotificationsEnabled(context = context))
                 activityResultLauncher.launch(android.Manifest.permission.POST_NOTIFICATIONS)
         }
 
         private fun areNotificationsEnabled(context: Context): Boolean {
+            if (Build.VERSION.SDK_INT < 33)
+                return true
             return ContextCompat.checkSelfPermission(
                 context, android.Manifest.permission.POST_NOTIFICATIONS
             ) == PackageManager.PERMISSION_GRANTED
