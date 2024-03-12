69debabd9b7f60f0a10a61b66deecf9957d0946a
Björn <bjoernwe@users.noreply.github.com>
2024-03-12 18:36:41 +0100

    ♻️ Remove redundant dispatcher

--- app/src/main/java/dev/upaya/shf/data/preferences/PreferencesDataSource.kt
+++ app/src/main/java/dev/upaya/shf/data/preferences/PreferencesDataSource.kt
@@ -8,8 +8,6 @@ import androidx.datastore.preferences.core.edit
 import androidx.datastore.preferences.preferencesDataStore
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dev.upaya.shf.data.ApplicationCoroutineScope
-import dev.upaya.shf.data.IoDispatcher
-import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
@@ -26,14 +24,13 @@ private val prefKeyPacingEnabled = booleanPreferencesKey("pacing_enabled")
 class PreferencesDataSource @Inject constructor(
     @ApplicationContext private val appContext: Context,
     @ApplicationCoroutineScope private val externalScope: CoroutineScope,
-    @IoDispatcher private val ioDispatcher: CoroutineDispatcher,
 ) {
 
     val isPacingEnabled: Flow<Boolean> = appContext
         .dataStore.data.map { prefs -> prefs[prefKeyPacingEnabled] ?: false }
 
     fun setPacingPreference(enabled: Boolean) {
-        externalScope.launch(ioDispatcher) { // make function main-safe
+        externalScope.launch { // make function main-safe
             appContext.dataStore.edit { settings ->
                 settings[prefKeyPacingEnabled] = enabled
             }
