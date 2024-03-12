c2e68e524ab45e45d10d9c1d5b87a2b40dac407f
Björn <bjoernwe@users.noreply.github.com>
2024-03-12 18:29:06 +0100

    ♻️ Simplify code

--- app/src/main/java/dev/upaya/shf/ui/LabelMapSHF.kt
+++ app/src/main/java/dev/upaya/shf/ui/LabelMapSHF.kt
@@ -5,18 +5,16 @@ import dev.upaya.shf.data.input.InputKey
 
 class LabelMapSHF {
     companion object : LabelMap {
-        override fun getLabel(key: InputKey): Label {
-            return when(key) {
-                InputKey.KEY_LEFT -> labelSee
-                InputKey.KEY_RIGHT -> labelHear
-                InputKey.KEY_DOWN -> labelFeel
-                InputKey.KEY_UP -> labelGone
-                InputKey.KEY_A -> labelSee
-                InputKey.KEY_B -> labelHear
-                InputKey.KEY_X -> labelGone
-                InputKey.KEY_Y -> labelFeel
-                else -> labelOther
-            }
+        override fun getLabel(key: InputKey) = when(key) {
+            InputKey.KEY_LEFT -> labelSee
+            InputKey.KEY_RIGHT -> labelHear
+            InputKey.KEY_DOWN -> labelFeel
+            InputKey.KEY_UP -> labelGone
+            InputKey.KEY_A -> labelSee
+            InputKey.KEY_B -> labelHear
+            InputKey.KEY_X -> labelGone
+            InputKey.KEY_Y -> labelFeel
+            else -> labelOther
         }
     }
 }
