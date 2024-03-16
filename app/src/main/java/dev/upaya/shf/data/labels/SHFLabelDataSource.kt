7f6b0a6cbd6641f475db86175a4cbcd2ae8d2b7b
Björn <bjoernwe@users.noreply.github.com>
2024-03-16 14:53:10 +0100

    🐛 Fix incorrect initial event

--- app/src/main/java/dev/upaya/shf/data/labels/SHFLabelDataSource.kt
+++ app/src/main/java/dev/upaya/shf/data/labels/SHFLabelDataSource.kt
@@ -2,6 +2,7 @@ package dev.upaya.shf.data.labels
 
 import dev.upaya.shf.data.gamepad.GamepadKeyEventDataSource
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.drop
 import kotlinx.coroutines.flow.transform
 import javax.inject.Inject
 import javax.inject.Singleton
@@ -11,7 +12,9 @@ import javax.inject.Singleton
 class SHFLabelDataSource @Inject constructor(
     gamepadKeyEventDataSource: GamepadKeyEventDataSource,
 ) {
-    val labelFlow: Flow<SHFLabelEvent> = gamepadKeyEventDataSource.inputKeyDown.transform { keyEvent ->
+    val labelFlow: Flow<SHFLabelEvent> = gamepadKeyEventDataSource.inputKeyDown
+        .drop(1)  // only emit the next actual event, not the current state
+        .transform { keyEvent ->
         SHFLabelMap.getLabel(keyEvent.gamepadKey)?.let { label ->
             emit(SHFLabelEvent(label))
         }
