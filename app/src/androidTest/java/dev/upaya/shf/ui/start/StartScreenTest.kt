b1377c42479a63732f868dbb73e51a250d01fbcb
Björn <bjoernwe@users.noreply.github.com>
2024-03-22 18:12:25 +0100

    ✅ Add tests

---
+++ app/src/androidTest/java/dev/upaya/shf/ui/start/StartScreenTest.kt
@@ -0,0 +1,30 @@
+package dev.upaya.shf.ui.start
+
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithContentDescription
+import dev.upaya.shf.ui.theme.SHFTheme
+import org.junit.Rule
+import org.junit.Test
+
+class StartScreenTest {
+
+    @get:Rule
+    val composeTestRule = createComposeRule()
+    // use createAndroidComposeRule<YourActivity>() if you need access to
+    // an activity
+
+    @Test
+    fun startScreen_hasStartButton() {
+        composeTestRule.setContent {
+            SHFTheme {
+                StartScreen(
+                    onStartButtonClick = {},
+                    onSettingsButtonClick = {},
+                )
+            }
+        }
+
+        composeTestRule.onNodeWithContentDescription("Start Session").assertIsDisplayed()
+    }
+}
