f40d84d1175e80d8cad5f6ea618f6f6a91a6aa18
Björn <bjoernwe@users.noreply.github.com>
2024-06-21 15:23:54 +0200

    🎨 Simplify code

--- app/src/main/java/dev/upaya/shf/data/auth/AuthRepository.kt
+++ app/src/main/java/dev/upaya/shf/data/auth/AuthRepository.kt
@@ -1,7 +1,6 @@
 package dev.upaya.shf.data.auth
 
 import android.content.Context
-import com.google.firebase.auth.FirebaseAuth
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import javax.inject.Inject
@@ -9,47 +8,29 @@ import javax.inject.Singleton
 
 
 @Singleton
-class AuthRepository @Inject constructor() {
+class AuthRepository @Inject constructor(
+    private val firebaseAuthenticator: FirebaseAuthenticator,
+) {
 
-    private val firebaseAuth = FirebaseAuth.getInstance()
-
-    private val _logInStatus = MutableStateFlow(if (isFirebaseSignedIn()) LogInStatus.LOGGED_IN else LogInStatus.LOGGED_OUT)
+    private val _logInStatus = MutableStateFlow(firebaseAuthenticator.getLogInStatus())
     val logInStatus: StateFlow<LogInStatus> = _logInStatus
 
-    private val _userEmail = MutableStateFlow<String?>(null)
+    private val _userEmail = MutableStateFlow(firebaseAuthenticator.getUserEmail())
     val userEmail: StateFlow<String?> = _userEmail
 
-    init {
-        firebaseAuth.currentUser?.also { user -> _userEmail.value = user.email }
-    }
-
     suspend fun signIn(activityContext: Context) {
-
-        if (_logInStatus.value != LogInStatus.LOGGED_OUT)
-            return
-
         _logInStatus.value = LogInStatus.LOGGING_IN
-        val googleCredential = signInWithGoogle(activityContext) ?: return
-
-        firebaseAuth.signInWithGoogleCredential(googleCredential)
-            .addOnSuccessListener {
-                _userEmail.value = it.user?.email
-                _logInStatus.value = LogInStatus.LOGGED_IN
-            }
-            .addOnFailureListener {
-                _logInStatus.value = LogInStatus.LOGGED_OUT
-            }
+        _logInStatus.value = firebaseAuthenticator.trySignIn(activityContext)
+        updateUserInfoFlows()
     }
 
     suspend fun signOut(activityContext: Context) {
         _logInStatus.value = LogInStatus.LOGGING_OUT
-        firebaseAuth.signOut()
-        _userEmail.value = firebaseAuth.currentUser?.email
-        signOutFromGoogle(activityContext)
-        _logInStatus.value = LogInStatus.LOGGED_OUT
+        _logInStatus.value = firebaseAuthenticator.signOut(activityContext)
+        updateUserInfoFlows()
     }
 
-    private fun isFirebaseSignedIn(): Boolean {
-        return firebaseAuth.currentUser != null
+    private fun updateUserInfoFlows() {
+        _userEmail.value = firebaseAuthenticator.getUserEmail()
     }
 }
